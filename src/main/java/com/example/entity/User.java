26a3d62ef0d9633f8d12ab6eee0f568dd522a4cd
OlPermiakova <olpermiakovaha@gmail.com>
2023-06-03 20:35:30 +0300

    Ready

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,18 +1,43 @@
 package com.example.entity;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
 
 @Data
 @Entity
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String Name;
     private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<Article> getArticles() {
+        return articles;
+    }
+
     private String password;
     @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     private Set<Article> articles;
